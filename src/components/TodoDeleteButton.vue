77a5c2c40e6afdcc5bc3d444363721ab24c10034
redmaun <deb.il.dpkg@gmail.com>
2025-07-07 03:00:00 +0300

    first commit

---
+++ src/components/TodoDeleteButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="$emit('delete')">x</button>
+</template>
+
+<script setup></script>
+
+<style scoped>
+button {
+  padding: 0.1rem;
+  padding-left: 0.4rem;
+  padding-right: 0.4rem;
+  outline: none;
+  border: none;
+  border-radius: 5rem;
+  cursor: pointer;
+}
+</style>
