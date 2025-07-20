cafc023903c15ab27f7800d31fbdd8be304d9251
redmaun <deb.il.dpkg@gmail.com>
2025-07-21 01:12:31 +0300

    d

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,29 @@
-import { fileURLToPath, URL } from "node:url";
+// import { fileURLToPath, URL } from "node:url";
+
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+// import vueDevTools from "vite-plugin-vue-devtools";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/todo-list",
+//   plugins: [vue()], //vueDevTools()],
+//   build: {
+//     outDir: "dist", // default
+//     rollupOptions: {
+//       input: "index.html", // or another entry point
+//     },
+//   },
+//   // resolve: {
+//   //   alias: {
+//   //     "@": fileURLToPath(new URL("./src", import.meta.url)),
+//   //   },
+//   // },
+// });
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "todo-list",
-  plugins: [vue()], //vueDevTools()],
-  // resolve: {
-  //   alias: {
-  //     "@": fileURLToPath(new URL("./src", import.meta.url)),
-  //   },
-  // },
+  plugins: [vue()],
 });
